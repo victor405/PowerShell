6ff24cbf969b29b807a3b06a5575d972cfc546cc
victor405 <victor.mendoza.ok@gmail.com>
2020-07-14 20:02:41 -0500

    Create New-ComplexPassword.ps1
    Password Generator

---
+++ New-ComplexPassword.ps1
@@ -0,0 +1,74 @@
+﻿<#
+    Author  : Victor Mendoza
+    Version : 1
+    Date    : 7/14/2020
+#>
+
+[CmdletBinding()]
+Param
+(
+    [ValidateRange(10,1000)]
+    [int]$Length = 20,
+
+    [string]$Exclude,
+
+    [switch]$NoRepeat
+)
+    
+Begin
+{
+    $charPool, $password = [System.Collections.ArrayList]@(), [System.Collections.ArrayList]@()
+    $characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()-_=+"
+
+    if ($exclude)
+    {
+        $excludeArray = $exclude.ToCharArray() | Sort-Object -Unique
+            
+        foreach ($char in $characters.ToCharArray())
+        {
+            if (!($excludeArray | Where-Object { $_ -like "*$char*" }))
+            {
+                $charPool.Add($char) | Out-Null
+            }
+        }
+    }
+    else
+    {
+        foreach ($char in $characters.ToCharArray())
+        {
+            $charPool.Add($char) | Out-Null
+        }        
+    }
+}
+Process
+{
+    try
+    {            
+        if ($charPool.Count -lt 2)
+        {
+            return "No Complex Result Possible"
+        }
+        else
+        {
+            for ($i = 0; $i -lt $length; $i++)
+            {
+                do
+                {
+                    $currentChar = ($charPool | Get-Random)
+                } while ($noRepeat -and $i -gt 0 -and $isLastChar -eq $currentChar)
+                $password.Add($currentChar) | Out-Null
+                $isLastChar = $currentChar
+            }
+
+            return ($password -join(""))
+        }
+    }
+    catch
+    {
+        Write-Error ""
+    }
+}
+End
+{
+    $charPool.Clear()
+}
